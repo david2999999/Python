c2b463bb0ceb78688e5d843c389f93df52f12b21
David Jiang <djiang86@binghamton.edu>
2018-05-24 01:03:45 +0000

    completed with tuples

---
+++ PDF/Variables/Variable.py
@@ -0,0 +1,25 @@
+def main():
+    first_string = "This is a string"
+    second_string = "This is another string"
+    first_number = 4
+    second_number = 5
+    print("The first variables are %s, %s, %d, %d" % (first_string, second_string, first_number, second_number))
+
+    proverb = "A penny saved"
+    proverb = proverb + " is a penny earned"
+    print(proverb)
+
+    pennies_saved = 0
+    pennies_saved = pennies_saved + 1
+    print(pennies_saved)
+
+    print(pennies_saved + 1)
+
+
+if __name__ == "__main__":
+    main()
+
+# and, as, assert, break, class, continue, def, del, elif, else,
+# except, exec,False, finally, for, from, global, if, import, in,
+# is, lambda, not, None, or, pass, print, raise, return, try, True,
+# while, with, yield
