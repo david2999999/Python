f60a46a3e8188b55de80b6b29f1485f6aa8f4879
David Jiang <djiang86@binghamton.edu>
2018-06-03 15:35:04 +0000

    Kitchen package to contain multiple classes

---
+++ PDF/Modules/Kitchen/Omelet.py
@@ -0,0 +1,48 @@
+class Omelet:
+    """This class creates an omelet object. An omelet can be in one of two states: ingredients or cooked,
+    An omlet object has the following interfaces
+    get_kind() - returns a string with the type of omelet
+    set_kind(kind) - returns the omelet to be the type named
+    set_new_kind(kind, ingredients) - lets you create an omelet
+    mix() - gets called after all the ingredients are gathered from the fridge
+    cook() - cook the omelet"""
+    def __init__(self, recipe , kind="cheese"):
+        """__init__ initializes the Omelet class to default to a cheese omelet"""
+        self.set_kind(kind)
+        self.recipe = recipe
+        return
+    def __ingredients__(self):
+        """Internal method to be called by a fridge or other objects that need to act on ingredients"""
+        return self.needed_ingredients
+    def get_kind(self):
+        return self.kind
+    def set_kind(self, kind):
+        possible_ingredients = self.__known_kinds(kind)
+        if possible_ingredients == False:
+            return False
+        else:
+            self.kind = kind
+            self.needed_ingredients = possible_ingredients
+    def set_new_kind(self, name, ingredients):
+        self.kind = name
+        self.needed_ingredients = ingredients
+        return
+    def __known_kinds(kind):
+        if kind == "cheese":
+            return {"eggs": 2, "milk": 1, "cheese": 1}
+        elif kind == "mushroom":
+            return {"eggs": 2, "milk": 1, "cheese": 1, "mushroom": 2}
+        elif kind == "onion":
+            return {"eggs": 2, "milk": 1, "cheese": 1, "onion": 1}
+        else:
+            return False
+    def get_ingredients(self, fridge):
+        self.from_fridge = fridge.get_ingredients(self)
+    def mix(self):
+        for ingredient in self.from_fridge.keys():
+            print("Mixing %d %s for the %s omelet" % (self.from_fridge[ingredient], ingredient, self.kind))
+        self.mixed = True
+    def make(self):
+        if self.mixed:
+            print("Cooking the %s omelet" % self.kind)
+            self.cooked = True
