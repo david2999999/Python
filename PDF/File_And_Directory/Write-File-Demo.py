882c6275d0dfc6f80ce188a819346f74f9383847
David Jiang <djiang86@binghamton.edu>
2018-10-16 20:02:16 +0000

    checking if the file already exist using os

--- PDF/File_And_Directory/Write-File-Demo.py
+++ PDF/File_And_Directory/Write-File-Demo.py
@@ -1,7 +1,17 @@
+import os
+
 def make_text_file():
     a = open('test.txt', "w")
     a.write("This is used to create a new text file")
     a.close()
 
 
+def make_another_file():
+    if os.path.isfile('test.txt'):
+        print("you are trying to create a file that already exist")
+    else:
+        f = open('text.txt', "w")
+        f.write("This is how you create a new text file")
+
 make_text_file()
+make_another_file()
