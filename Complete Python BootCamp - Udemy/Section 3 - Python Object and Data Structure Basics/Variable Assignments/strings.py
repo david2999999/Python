0c11110c5ae5f128b47e467a47a981a3fa230546
David Jiang <djiang86@binghamton.edu>
2020-03-28 10:53:36 -0400

    slicing and index a string

--- Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Variable Assignments/strings.py
+++ Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Variable Assignments/strings.py
@@ -4,4 +4,11 @@
     # "Don't"
 # Since strings are ordered sequences, it means we can use indexing and slice to grab sub-sections of the string
     # Indexing notation uses [] notation after the string (or variable assigned to the string)
-    # Indexing allows you to grab a single character from the string
+        # Indexing allows you to grab a single character from the string
+            # Each character has a number index to indicate positions of what you wish to grab
+            # There is also reverse index, where -1 is the position of the last character
+    # Slicing allows you to grab a subsection of multiple characters
+        # The has the following syntax" [start:stop:step]
+            # start is the numerical index for the slice start
+            # stop is the index you will go up to (but not include)
+            # step is the size of the "jump" you take
