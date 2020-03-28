797ccc5b0cae98b48538097798b67cf51618002a
David Jiang <djiang86@binghamton.edu>
2020-03-28 10:48:48 -0400

    strings and indexing of strings

---
+++ Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Variable Assignments/strings.py
@@ -0,0 +1,7 @@
+# Strings are sequences of characters, using the syntax of either single quotes or double quotes
+    # 'hello'
+    # "Hello"
+    # "Don't"
+# Since strings are ordered sequences, it means we can use indexing and slice to grab sub-sections of the string
+    # Indexing notation uses [] notation after the string (or variable assigned to the string)
+    # Indexing allows you to grab a single character from the string
