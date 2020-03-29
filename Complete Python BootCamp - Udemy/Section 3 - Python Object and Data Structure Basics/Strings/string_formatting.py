7a311178ec9f155c280102f6a9c3f2295cea17d6
David Jiang <djiang86@binghamton.edu>
2020-03-29 12:51:27 -0400

    string interpolation with format()

---
+++ Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Strings/string_formatting.py
@@ -0,0 +1,10 @@
+# Formatting with the .format() method
+print('This is a string {}'.format('INSERT'))
+
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+
+print('The {0} {0} {0}'.format('fox', 'brown', 'quick'))
+
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+print('The {f} {f} {f}'.format(f='fox', b='brown', q='quick'))
