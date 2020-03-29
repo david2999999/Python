907b1bd40c6fea88c8d45500ca2ae89ab26e2ccd
David Jiang <djiang86@binghamton.edu>
2020-03-29 12:59:11 -0400

    formatting floating point numbers

---
+++ Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Strings/Formatting/float_formatting.py
@@ -0,0 +1,8 @@
+result = 100/777
+print(result)
+print("The result was {}".format(result))
+print("The result was {r:1.3f}".format(r=result))
+
+result = 104.12345
+print("The result was {}".format(result))
+print("The result was {r:1.6f}".format(r=result))
