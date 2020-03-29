e9d6d28a1f6dc25801645f02751e2327b5bcd084
David Jiang <djiang86@binghamton.edu>
2020-03-29 12:14:53 -0400

    string concatenations

---
+++ Complete Python BootCamp - Udemy/Section 3 - Python Object and Data Structure Basics/Strings/properties_and_methods.py
@@ -0,0 +1,19 @@
+name = "Sam"
+# name[0] = 'P' - TypeError: 'str' object does not support item assignment
+
+last_letters = name[1:]
+print(last_letters)
+
+last_letters = 'P' + last_letters
+print(last_letters)
+
+hello_world = "Hello World"
+hello_world = hello_world + " it is beautiful outside"
+print(hello_world)
+
+hello_world += hello_world
+print(hello_world)
+
+letter = 'z'
+print(letter * 10)
+
