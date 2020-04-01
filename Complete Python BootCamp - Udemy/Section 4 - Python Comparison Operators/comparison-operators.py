0f93fa42be07dee43e7b3b7635a3090ff8e5f6ed
David Jiang <djiang86@binghamton.edu>
2020-04-01 05:30:39 -0400

    comparison operators

---
+++ Complete Python BootCamp - Udemy/Section 4 - Python Comparison Operators/comparison-operators.py
@@ -0,0 +1,16 @@
+print(2 == 2) # True
+print(2 == 1) # False
+print('hello' == 'bye') # False
+print('Bye' == 'bye') # False
+print('2' == 2) # False
+print(2.0 == 2) # True
+
+print(3 != 3) # False
+print(4 != 5) # True
+
+print(2 > 1) # True
+print(1 > 2) # False
+print(1 < 2) # True
+print(2 < 5) # True
+print(2 >= 2) # True
+print(4 <= 1) # False
