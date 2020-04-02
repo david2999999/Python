1459d6cca3323baf5daf92ca73f74bc4957727a5
David Jiang <djiang86@binghamton.edu>
2020-04-02 05:48:52 -0400

    if/elif/else example

---
+++ Complete Python BootCamp - Udemy/Section 5 - Python Statements/if-elif-else.py
@@ -0,0 +1,30 @@
+if 3 > 2:
+    print('It is true')
+
+hungry = True
+if hungry:
+    print("Feed the dog")
+
+hungry = False
+if hungry:
+    print("Feed the dog")
+else:
+    print("Don't need to feed the dog")
+
+loc = 'Store'
+if loc == 'Auto Shop':
+    print("Time to buy a car")
+elif loc == 'Bank':
+    print("Time to deposit money")
+elif loc == 'Store':
+    print("Welcome to the store")
+else:
+    print("Undefined location")
+
+name = 'Sammy'
+if name == 'Frankie':
+    print("Hello Frankie")
+elif name == 'Sammy':
+    print("Hello Sammy")
+else:
+    print("Unidentified person")
