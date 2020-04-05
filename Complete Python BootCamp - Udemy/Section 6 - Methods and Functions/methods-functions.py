28baee1a55eb3a780d40b7fa5890871b85b271f5
David Jiang <djiang86@binghamton.edu>
2020-04-05 09:18:44 -0400

    simple function examples

--- Complete Python BootCamp - Udemy/Section 6 - Methods and Functions/methods-functions.py
+++ Complete Python BootCamp - Udemy/Section 6 - Methods and Functions/methods-functions.py
@@ -2,3 +2,27 @@
 my_list = [1, 2, 3]
 my_list.append(4) # method in list
 my_list.pop() # method in list
+
+def name_function():
+    """
+    DOCSTRING: Information about the function
+    Input: No input..
+    Output: Hello ...
+    """
+    print('hello')
+
+help(name_function)
+name_function()
+####################################################
+def say_hello(name='NAME'):
+    return 'Hello ' + name
+
+result = say_hello()
+print(result)
+result = say_hello('Sally')
+print(result)
+####################################################
+def add(n1, n2):
+    return n1 + n2
+
+print(add(20, 30))
